fbf81e1adf69860f3e22e500f016215f359b2c80
Madelyn Grace Nelson <mnelso12@student00.cse.nd.edu>
2016-02-25 02:09:27 -0500

    things are working

--- Makefile
+++ Makefile
@@ -15,6 +15,21 @@ test: $(TARGETS)
 	echo '(println 1738)' > testfile2
 	./schemeterpreter testfile2
 	rm testfile2
+	echo '(println 1738' > testfile3
+	./schemeterpreter testfile3
+	rm testfile3
+	echo '(print)' > testfile4
+	./schemeterpreter testfile4
+	rm testfile4
+	echo '(print (print (print 'print')))' > testfile5
+	./schemeterpreter testfile5
+	rm testfile5
+	echo '(print a (print b))' > testfile6
+	./schemeterpreter testfile6
+	rm testfile6
+	echo '(print     'he123lo'  )' > testfile7
+	./schemeterpreter testfile7
+	rm testfile7
 
 clean:
 	rm -f *.o $(TARGETS)
--- parser.c
+++ parser.c
@@ -18,6 +18,17 @@ void pushStack(Token t, Stack *s) {
     s->count++;
 }
 
+Token topOfStack(Stack *s) {
+    /*
+     * Tokens are pushed onto the stack via shallow copy
+     * In order to simulate stack, 
+     * it's easier to put things at the back of the array.
+     */
+    // TODO deal with stack being too small
+    return s->buffer[s->count - 1];
+}
+
+
 Token popStack(Stack *s) {
     /*
      * Tokens are popped off the stack via shallow copy
@@ -42,84 +53,115 @@ void runParser(InputBuffer *ib) {
     t.content = NULL;
     pushStack(t, &st);
     // Push Start, pop start push stmt
-    t.type = STMT;
-    t.content = NULL;
+//    t.type = STMT;
+//    t.content = NULL;
+//    pushStack(t, &st);
+
+    t.type = RIGHTPAREN;
+    pushStack(t, &st);
+    t.type = ARGS;
+    pushStack(t, &st);
+    t.type = IDENT;
+    pushStack(t, &st);
+    t.type = LEFTPAREN;
     pushStack(t, &st);
 
     // Read through all symbols in input buffer and parse
     for (i = 0; i < ib->bufcount; i++) {
-        if (st.buffer[i].type == STMT) {
-            popStack(&st);
-            
-            t.type = RIGHTPAREN;
-            //t.content = ")";
-            pushStack(t, &st);
-            
-            t.type = ARGS;
-            //t.content = NULL;
-            pushStack(t, &st);
-            
-            t.type = IDENT;
-            //t.content = NULL;
-            pushStack(t, &st);
-            
-            t.type = LEFTPAREN;
-            //t.content = "(";
-            pushStack(t, &st);
+        //printf("%i\n", ib->buffer[i].type);
+        
+
+        if (ib->buffer[i].type == IDENT) {
+            if (topOfStack(&st).type == IDENT) {
+                popStack(&st);
+            }
+            else {
+                printf("invalid syntax 0\n");
+                return;
+            }
         }
-        else if (st.buffer[i].type == IDENT) {
+        else if (ib->buffer[i].type == STRING) {
            popStack(&st);  
         }
-        else if (st.buffer[i].type == STRING) {
+        else if (ib->buffer[i].type == NUMBER) {
            popStack(&st);  
         }
-        else if (st.buffer[i].type == NUMBER) {
-           popStack(&st);  
-        }
-        else if (st.buffer[i].type == ARGS) {
-            popStack(&st);
+        else if (ib->buffer[i].type == ARGS) {
+/*            popStack(&st);
             t.type = ARGS;
             pushStack(t, &st); 
-            if (st.buffer[i+1].type == IDENT) {
+            if (ib->buffer[i+1].type == IDENT) {
                 t.type = IDENT;
                 pushStack(t, &st);
             } 
-            else if (st.buffer[i+1].type == STRING) {
+            else if (ib->buffer[i+1].type == STRING) {
                 t.type = STRING;
                 pushStack(t, &st);
             }
-            else if (st.buffer[i+1].type == NUMBER) {
+            else if (ib->buffer[i+1].type == NUMBER) {
                 t.type = NUMBER;
                 pushStack(t, &st);
             }
-            else if (st.buffer[i+1].type == RIGHTPAREN) {
-                t.type = RIGHTPAREN;
-                pushStack(t, &st);
+            else if (ib->buffer[i+1].type == RIGHTPAREN) {
+                popStack(&st);
             }
-            else if (st.buffer[i+1].type == LEFTPAREN) {
+            else if (ib->buffer[i+1].type == LEFTPAREN) {
                 t.type = LEFTPAREN;
                 pushStack(t, &st);
             }
-            else if (st.buffer[i+1].type == WS) {
+            else if (ib->buffer[i+1].type == WS) {
                 t.type = WS;
+                pushStack(t, &st);
+            }
+            else if (ib->buffer[i+1].type == STMT) {
+                t.type = STMT;
                 pushStack(t, &st);
             }
             else {
                 printf("Error. Invalid Syntax. \n");
                 return;
             }
+ */
         }
-        else if (st.buffer[i].type == DOLLAR) {
-            
+        else if (ib->buffer[i].type == LEFTPAREN) {
+            if (topOfStack(&st).type == STMT) {
+                t.type = RIGHTPAREN;
+                pushStack(t, &st);
+                t.type = ARGS;
+                pushStack(t, &st);
+                t.type = IDENT;
+                pushStack(t, &st);
+            }
+            else if (topOfStack(&st).type == LEFTPAREN) {
+                popStack(&st);
+            }
         }
-        else if (st.buffer[i].type == LEFTPAREN) {
-            popStack(&st);    
+        else if (ib->buffer[i].type == RIGHTPAREN) {
+            if (topOfStack(&st).type == ARGS) {
+                popStack(&st); // pop args off stack
+                i--; // so we can check again for more args
+            }
+            else if (topOfStack(&st).type == RIGHTPAREN) {
+                popStack(&st);
+            }
+            else {
+                printf("in else if right paren\n");
+            }
         }
-        else if (st.buffer[i].type == RIGHTPAREN) {
-            
-        }
-        else if (st.buffer[i].type == WS) {
-            
+        else if (ib->buffer[i].type == WS) {
+            // do nothing    
         } 
+        else {
+            printf("error. invalid syntax. 9\n");
+            return;
+        }
+
+        if (topOfStack(&st).type == DOLLAR) {
+            printf("Success!!!\n");
+            return;
+        }
+        
     }
+    printf("error. invalid syntax. 10 \n");
+    return;
 }

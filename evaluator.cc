9dc8db0c5bc886b2155de8205d584620175f183b
reds0l <jrodger4@nd.edu>
2016-03-22 02:28:29 -0400

    now print works

--- evaluator.cc
+++ evaluator.cc
@@ -8,15 +8,15 @@
 
 #include "evaluator.h"
 
-    // check ident
-    // if stmt.value != null
-    //   evalstmt(stmt) !!!make dat function!!
-    // if string or number != null
-    //   evalarg()
-    //   do something based on string/num
-    //   if nextArg != null
-    //     evalarg()
-    
+// check ident
+// if stmt.value != null
+//   evalstmt(stmt) !!!make dat function!!
+// if string or number != null
+//   evalarg()
+//   do something based on string/num
+//   if nextArg != null
+//     evalarg()
+
 void runEvaluator(stmt *stmt_head) {
     evalStmt(stmt_head);
 }
@@ -26,58 +26,50 @@ void evalStmt (stmt *head) {
     stmt *temp_stmt = head;
     args *temp_arg = head->stmtArgs;
 
-    std::cout << "before println if statment" << std::endl;
-    std::cout << head->ident << std::endl;
     if (head->ident == "println") {
-        std::cout << "dat if tho" << std::endl;
-        do{
-            //if (temp_stmt->stmtArgs != NULL)
+        while(temp_arg->nextArg !=NULL)
+        {
+            if (temp_arg->argNum != NULL)
             {
-                std::cout << "n'do" << std::endl;
-                if (temp_arg->argNum != NULL)
-                {
-                    std::cout << "in arg num" << std::endl;
-                    std::cout << temp_arg->argNum << std::endl;
-                }
-
-                if (temp_arg->argString != NULL)
-                {
-                    std::cout << "in arg str" << std::endl;
-                    std::cout << temp_arg->argString << std::endl;
-                }
-
-                if (temp_arg->argStmt != NULL)
-                {
-                    std::cout << "in arg stmt" << std::endl;
-                    evalStmt(temp_arg->argStmt);
-                }
+                std::cout << temp_arg->argNum << std::endl;
             }
-        }while (temp_arg->nextArg != NULL);
+
+            if (temp_arg->argString != NULL)
+            {
+                std::cout << *(temp_arg->argString) << std::endl;
+            }
+
+            if (temp_arg->argStmt != NULL)
+            {
+                evalStmt(temp_arg->argStmt);
+            }
+            temp_arg = temp_arg->nextArg;
+        }
     }
 }
 
 /*
-std::string evalArg (args *arg) {
-    std::string next_string;
-    std::string return_string;
+   std::string evalArg (args *arg) {
+   std::string next_string;
+   std::string return_string;
 
-    //if (arg->argStmt->value != NULL)
-    
+//if (arg->argStmt->value != NULL)
 
 
-    if (arg->argNum != NULL)
-    {
-        return_string = arg->argNum;
-    }
-    if (arg->argString != NULL)
-    {
-        return_string = arg->argString;
-    }
-    if (arg->nextArg != NULL)
-    {
-        next_string = evalArg(arg->nextArg);
-        return_string = return_string + next_string;
-    }
-    return return_string;
+
+if (arg->argNum != NULL)
+{
+return_string = arg->argNum;
+}
+if (arg->argString != NULL)
+{
+return_string = arg->argString;
+}
+if (arg->nextArg != NULL)
+{
+next_string = evalArg(arg->nextArg);
+return_string = return_string + next_string;
+}
+return return_string;
 }
 */

41a476ba6ba4236fa453690a06d135315c930e8b
Charles Shinaver <bshinaver@gmail.com>
2016-04-18 03:56:12 -0400

    Turing machine adds now

--- evaluator.cc
+++ evaluator.cc
@@ -9,6 +9,37 @@
 
 #include "evaluator.h"
 
+void print_add_turing_machine_with_args(int x, int y) {
+    std::cout << "{" << std::endl;
+
+    std::cout << "('q0',''):('q1','#','R'),\n";
+    for (int i = 1; i < x + 1; i++) {
+        std::cout << "('q" << i <<  "',''):('q" << (i + 1) << "','1','R'),\n";
+    }
+    std::cout << "('q" << (x + 1) << "',''):('q" << (x + 2) << "','+','R'),\n";
+    for (int i = x + 2; i < x + y + 2; i++) {
+        std::cout << "('q" << i <<  "',''):('q" << (i + 1) << "','1','R'),\n";
+    }
+    std::cout << "('q" << (x + y + 2) << "',''):('leftMost','=','L'),\n";
+
+
+    // Add machine
+    std::cout << "('w1','1'):('rightMost','x','R'),\n"
+        "('w1','+'):('w1','x','R'),\n"
+        "('w1','='):('qa','=','R'),\n"
+        "('rightMost','1'):('rightMost','1','R'),\n"
+        "('rightMost','+'):('rightMost','+','R'),\n"
+        "('rightMost','='):('rightMost','=','R'),\n"
+        "('rightMost',''):('leftMost','1','L'),\n"
+        "('leftMost','1'):('leftMost','1','L'),\n"
+        "('leftMost','+'):('leftMost','+','L'),\n"
+        "('leftMost','='):('leftMost','=','L'),\n"
+        "('leftMost','x'):('w1','x','R'),\n"
+        "('leftMost','#'):('w1','#','R'),\n";
+    std::cout << "}" << std::endl;
+
+}
+
 void runEvaluator(std::vector<stmt *> stmts) {
     for (auto s : stmts) {
         evalStmt(s);
@@ -40,24 +71,9 @@ void evalStmt (stmt *head) {
         std::cout << std::endl;
     }
     else if (ident == "add") {
-        long double sum = 0;
-        while(temp_arg->nextArg !=NULL) {
-            if (temp_arg->argNum != NULL) {
-                sum += *(temp_arg->argNum);
-            }
-            else if (temp_arg->argStmt != NULL) {
-                evalStmt(temp_arg->argStmt);
-                sum += stold(temp_arg->argStmt->value);
-            }
-            else if (temp_arg->argString != NULL) {
-                okayErrorFunction(1,"Tried to add a string");
-            }
-
-            temp_arg = temp_arg->nextArg;
-        }
-        std::ostringstream strs;
-        strs << sum;
-        head->value = strs.str();
+        int x = *(temp_arg->argNum);
+        int y = *(temp_arg->nextArg->argNum);
+        print_add_turing_machine_with_args(x, y);
 
     }
     else if (ident == "sub") {

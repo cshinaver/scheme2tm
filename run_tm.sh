b8d834b05f61f9cb7c7b07c289a3d1ae34e669d7
cshinaver <bshinaver@gmail.com>
2016-04-19 21:51:06 -0400

    Added run_tm

---
+++ run_tm.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# This script will create a turing machine using
+# scheme2tm and execute it
+# using the python library turing_machine
+
+SCHEME_FILE=$1
+EXECUTABLE=schemeterpreter
+
+usage() {
+    cat <<EOF
+Usage: run_tm.sh scheme_file...
+EOF
+    exit 1
+}
+
+if ! [ -f $EXECUTABLE ]; then
+    echo "No $EXECUTABLE executable found"
+    echo
+    usage
+fi
+
+if ! pip freeze 2>/dev/null | grep turing-machine; then
+    cat <<EOF
+Please install the turing_machine python library
+with $ pip install turing_machine.
+EOF
+    exit 1
+fi
+
+
+cat $SCHEME_FILE | while read prog; do
+    tm=$(./$EXECUTABLE <(echo $prog))
+    echo "Running Turing machine for $prog..."
+    echo
+    python - <<EOF
+from turing_machine import TuringMachine
+
+tm = TuringMachine($tm)
+
+tm.debug('', step_limit=1000000000)
+EOF
+    echo
+done
+
